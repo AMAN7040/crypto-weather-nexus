38755abec3391edc4e59283ff43335b7ef4d8732
AMAN7040 <amansingh79620@gmail.com>
2025-04-01 22:34:48 +0530

    chore: initialize Crypto-weather project

---
+++ src/app/layout.js
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "CryptoWeather Nexus | Real-time Crypto & Weather Data",
+  description:
+    "Get real-time cryptocurrency prices and weather updates in one place. Stay informed with the latest trends and forecasts.",
+  keywords:
+    "cryptocurrency, weather, Bitcoin, Ethereum, real-time data, forecast",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
