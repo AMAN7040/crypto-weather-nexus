7a1304ab9a9da53828915f4d0dc93b890ee7467e
AMAN7040 <amansingh79620@gmail.com>
2025-04-04 16:55:16 +0530

    feat(notifications): Set up Redux store and actions for managing notifications

--- src/lib/store/store.js
+++ src/lib/store/store.js
@@ -2,9 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import weatherReducer from "./slices/weatherSice";
 import coinReducer from "./slices/coinSlice";
 import NewsReducer from "./slices/newsSlice";
+import notificationsReducer from "./slices/notificationSlice";
 
 export const makeStore = () => {
   return configureStore({
-    reducer: { weather: weatherReducer, coins: coinReducer, news: NewsReducer, },
+    reducer: {
+      weather: weatherReducer,
+      coins: coinReducer,
+      news: NewsReducer,
+      notifications: notificationsReducer,
+    },
   });
 };
