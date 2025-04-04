63c955b418115a6aa92c90d8d0ce3167fe87a743
AMAN7040 <amansingh79620@gmail.com>
2025-04-04 12:25:05 +0530

    feat(root page): update root layout to fetch and pass SSR data for hydration - Passed data to components via props to initialize Redux state

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,35 @@
-import Image from "next/image";
+import Head from "next/head";
+import Weather from "./components/Weather";
+import Crypto from "./components/Crypto";
+import News from "./components/News";
+import { getCities } from "./utils/getCities";
+import { fetchWeather } from "./utils/fetchWeather";
+import { fetchCoins } from "./utils/fetchCoins";
+import { fetchNews } from "./utils/fetchNews";
 
-export default function Home() {
+export default async function Home() {
+  const coins = await fetchCoins();
+  const cities = getCities();
+  const weatherData = await fetchWeather(cities);
+  const newsData = await fetchNews();
   return (
-    <div className="text-red-500">Let us start the project</div>
+    <>
+      <Head>
+        <title>CryptoWeather Nexus Dashboard</title>
+        <meta
+          name="description"
+          content="Monitor live weather and cryptocurrency data in one place."
+        />
+        <meta
+          name="keywords"
+          content="crypto, weather, dashboard, cryptocurrency, news"
+        />
+      </Head>
+      <main className="grid grid-cols-1 grid-rows-[2fr_350px_2fr] sm:grid-cols-[2fr_2fr_1.5fr] sm:grid-rows-[2fr_2fr_1.5fr] gap-3 h-[max(calc(100vh-66px))] py-5 px-2 md:px-6">
+        <Crypto coins={coins} />
+        <Weather initialData={weatherData} />
+        <News newsData={newsData} />
+      </main>
+    </>
   );
 }
