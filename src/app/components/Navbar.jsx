1cd74de3390a642df9c75de21182bf8a1539d382
AMAN7040 <amansingh79620@gmail.com>
2025-04-04 16:57:29 +0530

    feat(navbar): Integrate notifications panel with Navbar
    - Update Navbar to include Notification icon and alert count.
    - Implement toggle functionality to open/close the notifications panel.
    - Display real-time notifications count and messages in the panel.
    - Style the notification icon and panel for better UX.

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,31 +2,18 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Hamburger from "./Hamburger";
-
-const NotificationIcon = ({ size = 22 }) => (
-  <div className="relative">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      width={size}
-      height={size}
-      className="cursor-pointer hover:text-gray-300 transition-all duration-200 transform active:scale-110 "
-    >
-      <path d="M12 2C8.69 2 6 4.69 6 8v5H4v2h16v-2h-2V8c0-3.31-2.69-6-6-6zM10 19a2 2 0 104 0h-4z" />
-    </svg>
-    <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs w-3.5 h-3.5 flex items-center justify-center rounded-full">
-      3
-    </span>
-  </div>
-);
+import Notification from "./Notification";
 
 const Navbar = () => {
   return (
     <header className="shadow-md z-50 w-full flex justify-center h-16">
       <nav className="w-[92%] flex items-center justify-between py-4  ">
         <div className="my-auto w-1/3">
-          <Link href="/" aria-label="Home" className="text-white flex items-center gap-2 text-[max(2.2vw,25px)]">
+          <Link
+            href="/"
+            aria-label="Home"
+            className="text-white flex items-center gap-2 text-[max(2.2vw,25px)]"
+          >
             <Image
               src="/CWN_Logo.png"
               alt="Logo"
@@ -34,7 +21,7 @@ const Navbar = () => {
               height={46}
               priority
             />
-           ℂ𝕎ℕ
+            ℂ𝕎ℕ
           </Link>
         </div>
 
@@ -60,7 +47,7 @@ const Navbar = () => {
         </div>
 
         <div className="flex items-center justify-between w-1/7 md:w-1/3 md:justify-end">
-          <NotificationIcon />
+          <Notification />
           <Hamburger />
         </div>
       </nav>
