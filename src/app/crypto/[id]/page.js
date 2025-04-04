bd1c434fb99f07bb8cf386229da85515a708784a
AMAN7040 <amansingh79620@gmail.com>
2025-04-04 18:49:03 +0530

    update(crypto-page) with the crypto detais

--- src/app/crypto/[id]/page.js
+++ src/app/crypto/[id]/page.js
@@ -13,12 +13,32 @@ const page = async ({ params }) => {
   return (
     <div className="flex justify-center items-center p-4">
       <div className="flex flex-col justify-center items-center border text-white border-gray-500 p-4 rounded-lg max-w-[600px]">
-        <h1 className="underline pb-4">{coin?.name} Details</h1>
-        <p className="text-lg md:text-xl">{coin?.symbol.toUpperCase()}</p>
-        <h2 className="text-sm md:text-lg font-bold bg-green-500 rounnded-md p-2">
-          Price: ${coin?.current_price}
+        <h1 className="underline pb-4 text-xl md:text-2xl font-bold">
+          {coin?.name} ({coin?.symbol.toUpperCase()}) Details
+        </h1>
+
+        <h2 className="text-sm md:text-lg font-bold bg-green-500 rounded-md p-2 my-2">
+          Price: ${coin?.market_data?.current_price?.toLocaleString()}
         </h2>
-        <p className="text-xs py-2 md:text-sm">{coin?.description}</p>
+
+        <p className="text-sm md:text-base">
+          24h Change:{" "}
+          <span
+            className={`font-bold ${
+              coin?.priceChange24h > 0 ? "text-green-500" : "text-red-500"
+            }`}
+          >
+            {coin?.priceChange24h.toFixed(2)}%
+          </span>
+        </p>
+
+        <p className="text-sm md:text-base">
+          Market Cap: ${coin?.marketCap.toLocaleString()}
+        </p>
+
+        <p className="text-xs py-2 md:text-sm">
+          {coin?.description || "No description available."}
+        </p>
       </div>
     </div>
   );
