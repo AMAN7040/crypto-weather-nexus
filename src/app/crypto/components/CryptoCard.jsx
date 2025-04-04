5b27f11828a9325595730bf60cb4ce116923393a
AMAN7040 <amansingh79620@gmail.com>
2025-04-04 22:51:25 +0530

    feat(crypto-card): create the favourite functionaity

--- src/app/crypto/components/CryptoCard.jsx
+++ src/app/crypto/components/CryptoCard.jsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
+import dynamic from "next/dynamic";
 import React from "react";
+import CryptoFavoriteToggle from "./CryptoFavoriteToggle";
 
-const CryptoCard = ({crypto}) => {
+const CryptoCard = ({ crypto }) => {
   return (
-    <section className="col-span-1 row-span-1 md:row-span-2 border border-gray-600 text-center p-3 md:p-4 rounded-lg">
-      <Link href={`/crypto/${crypto?.id}`}>
-        <h2 className="font-semibold text-sm md:text-lg text-blue-600 hover:underline">
-          {crypto?.name} ({crypto?.symbol.toUpperCase()})
+    <section className="relative col-span-1 row-span-1 md:row-span-2 border border-gray-600 text-center p-3 md:p-4 rounded-lg bg-[#1b1b1d] hover:bg-[#272a30] transition">
+      <Link href={`/crypto/${crypto.id}`}>
+        <h2 className="font-semibold text-sm md:text-lg text-blue-400 hover:underline">
+          {crypto.name} ({crypto.symbol.toUpperCase()})
         </h2>
-        <h3 className="text-xs md:text-sm">{crypto?.name}</h3>
       </Link>
+
+      <CryptoFavoriteToggle id={crypto.id} />
     </section>
   );
 };
