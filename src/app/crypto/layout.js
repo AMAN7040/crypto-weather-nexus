200715b495d7b6b501233085a109c5a206a9146d
AMAN7040 <amansingh79620@gmail.com>
2025-04-04 18:48:20 +0530

    update(crytp-layout) structure

--- src/app/crypto/layout.js
+++ src/app/crypto/layout.js
@@ -1,21 +1,16 @@
 export const metadata = {
-    title: "CryptoCurrency | Real-time Crypto Data",
-    description:
-      "Get real-time cryptocurrency Details.",
-    keywords:
-      "cryptocurrency, Crypto, Bitcoin, Ethereum, real-time data",
-  };
+  title: "CryptoCurrency | Real-time Crypto Data",
+  description: "Get real-time cryptocurrency Details.",
+  keywords: "cryptocurrency, Crypto, Bitcoin, Ethereum, real-time data",
+};
 
-  export default function Layout({ children }) {
-
-
-    return (
-      <html lang="en">
-        <body>
-          <h1 className="text-center py-3 text-white font-bold">THIS IS CRYPTO PAGE</h1>
-          {children}
-        </body>
-      </html>
-    );
-  } 
-  
+export default function Layout({ children }) {
+  return (
+    <main className="crypto-page">
+      <h1 className="text-center py-3 text-white font-bold">
+        THIS IS CRYPTO PAGE
+      </h1>
+      {children}
+    </main>
+  );
+}
