fb8d56798d8b4cfc711e952eecc5a942f4926be2
AMAN7040 <amansingh79620@gmail.com>
2025-04-05 21:39:21 +0530

    feat: add interactive rotating border overlay on crypto card hover

--- src/app/crypto/components/CryptoCard.jsx
+++ src/app/crypto/components/CryptoCard.jsx
@@ -5,9 +5,10 @@ import CryptoFavoriteToggle from "./CryptoFavoriteToggle";
 
 const CryptoCard = ({ crypto }) => {
   return (
-    <section className="relative col-span-1 row-span-1 md:row-span-2 border border-gray-600 text-center p-3 md:p-4 rounded-lg bg-[#1b1b1d] hover:bg-[#272a30] transition">
+    <section className="group relative col-span-1 row-span-1 md:row-span-2 border border-gray-600 text-center p-3 md:p-4 rounded-lg bg-[#1b1b1d] hover:bg-[#272a30] transition hover:scale-[1.1] hover:z-1 hover:border-0 ">
+      <div className="absolute inset-0 border-2 border-blue-400 rotate-[10deg] transition-all duration-300 ease-in-out opacity-0 group-hover:rotate-0 group-hover:inset-1  group-hover:opacity-[1] pointer-events-none"></div>
       <Link href={`/crypto/${crypto.id}`}>
-        <h2 className="font-semibold text-sm md:text-lg text-blue-400 hover:underline">
+        <h2 className="font-semibold text-sm md:text-lg text-blue-400 hover:underline hover:z-30">
           {crypto.name} ({crypto.symbol.toUpperCase()})
         </h2>
       </Link>
